8feda29edff645c4e458ec6251530bab2160076a
mehedi hasan <mhshohan04@gmail.com>
2025-04-18 11:15:08 +0600

    Context: an alternative to passing props

---
+++ src/Section.jsx
@@ -0,0 +1,13 @@
+
+import { LevelContext } from './context/LevelContext';
+
+
+export default function Section({ level, children }) {
+  return (
+    <section className="section">
+      <LevelContext value={level}>
+        {children}
+      </LevelContext>
+    </section>
+  );
+}
